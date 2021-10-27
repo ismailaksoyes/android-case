f4a385fecab74473168b396ff127aaa4523ab71c
ismailaksoyes <ismailaksoyes@gmail.com>
2021-10-27 20:07:36 +0300

    #18 Fix search behavior

--- app/src/main/java/com/avmogame/appcent/ui/home/HomeFragment.kt
+++ app/src/main/java/com/avmogame/appcent/ui/home/HomeFragment.kt
@@ -76,7 +76,7 @@ class HomeFragment : Fragment() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
             override fun afterTextChanged(editable: Editable?) {
-                if (editable.toString().length >= 2) {
+                if (editable.toString().length >= 3) {
                     sendSearchQuery(editable.toString())
                     setAdapterState(GameAdapterState.SEARCH_STATE)
                 } else if (editable.toString().isEmpty()) {
